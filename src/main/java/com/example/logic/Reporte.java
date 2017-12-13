e9b638678e18d885842b6a735ee846869b3bf3ab
davidhq0 <davidhq0@gmail.com>
2017-12-13 12:41:39 -0500

    creating the crearReportes method in the Reportes class

--- src/main/java/com/example/logic/Reporte.java
+++ src/main/java/com/example/logic/Reporte.java
@@ -1,6 +1,15 @@
 package com.example.logic;
+import java.util.*;
 
 //AQUI SE IMPLEMENTARAN REPORTES DE LA BASE DE DATOS PARA MOSTRAR SOLO EN CONSOLA
 public class Reporte {
 	String numReporte;
+	Calendar fecha;
+	
+	public Reporte() {
+		
+	}
+	public void crearReporte() {
+		System.out.println("Reporte de Clientes con sus Paquetes");
+	}
 }
