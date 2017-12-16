6f614a7002f5e5840d25468383ef933e3e7bc989
Alberth-SP <alberth.cb92@gmail.com>
2017-12-16 16:24:34 -0500

    thing

---
+++ src/main/webapp/resources/core/js/paquete.js
@@ -0,0 +1,78 @@
+var d = new Date();
+alert("FECHA: "+d.getDate()+" "+(d.getMonth()+1)+" "+d.getFullYear());
+
+function addPaquete(){	
+	
+	var url = "paquete/savePaquete";
+	//var image1 = $("#imagen1")[0].files[0];
+	//var image2 = $("#imagen2")[0].files[0];
+	
+	//var destinos = $('#multiselectDestino').val();	
+
+	var datas = new FormData();
+	var form_data = $('#form_regPaquete').serializeArray();
+	
+	$.each(form_data,function(key,input){
+		datas.append(input.name,input.value);
+	});
+	
+	//datas.append("image1", image1);
+	//datas.append("image2", image2);
+	//datas.append("destinos", destinos);
+	alert("dfsdf");
+	$.ajax({
+		url: url,
+		type: 'POST',
+		data: datas,  
+		enctype: 'multipart/form-data',
+		processData: false,
+		contentType: false,
+		success: function(result){
+			if(result){  
+
+				alert('sucess add Admin!' + result);                        
+			}
+			else{
+				alert('ocurrio algun ERROR, vuelva a intentarlo ');
+			}   
+
+		},
+		error: function(){
+			alert('Ocurrio un erro en el Proceso');
+		}
+	});
+}
+
+
+function changeStatePaquete(id,state){  
+	var url = "paquete/changeStatePaquete";
+	alert("vals: "+id + " "+state);
+
+	$.ajax({
+		url: url,
+		type: 'POST',
+		data: {"key": id, "state":state},      
+		success: function(result){
+			if(result){  
+				alert('sucess change State paquete!' + result);
+			}
+			else{
+				alert('ocurrio algun ERROR, vuelva a intentarlo ');
+
+			}   
+		},
+		error: function(){
+			alert('Ocurrio un error en el Proceso');
+		}
+	});
+}
+
+function changeCheckBox2(id, check){
+
+	var state = "0";
+	if(check.checked){
+		state="1";
+	}
+	changeStatePaquete(id, state);		
+}
+
