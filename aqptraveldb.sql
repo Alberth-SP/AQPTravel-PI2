73cdef187a539429e8e42d5f4d638be659a96a49
davidhq0 <davidhq0@gmail.com>
2017-12-25 17:42:02 -0500

    more data in agencia

--- aqptraveldb.sql
+++ aqptraveldb.sql
@@ -103,11 +103,10 @@ CREATE TABLE IF NOT EXISTS `agencia` (
 -- Volcado de datos para la tabla `agencia`
 --
 
-INSERT INTO `agencia` (`idAgencia`, `nombreAgencia`, `correoAgencia`, `rucAgencia`, `razonsocialAgencia`, `ubigeoAgencia`, `direccionAgencia`, `valoracionAgencia`, `descripcionAgencia`, `telefonoAgencia`, `diaModAgencia`, `mesModAgencia`, `anioModAgencia`, `idAdmin`, `contrasenaAgencia`, `estadoAgencia`) VALUES ('001', 'Colca Tours SA', 'colcaT@gmail.com', '234512233212', 'Viajes y muestra de la diversidad cultural del pais', '1234656312', 'av. el sol 342', '3', 'La agencia consta con terminal propio y una atencion y flota de buses de primera', '239812', 12, 12, 2017, '2', 'universo123', '1'),
-('002', 'Colca Travels SA', 'tarvelColca@gmail.com', '434512443212', 'Especialista en viajescon fines culturales ', '12335632', 'av. las torres 202', '2', 'La agencia consta con terminal propio y una atencion de primera', '258412', 12, 18, 2017, '3', 'universo', '1'),
-
-(4, 'Tours Peru', 'toursperu@gmail.com', '09213245', 'Viajes,turismo y aventura', '9021343222', 'av. los geranios 112', 3, 'Agencia esta al alcance de sus destinos mas cercanos', '783221', 11, 10, 2016, 28, 'qwerty', '1');
-
+INSERT INTO `agencia` (`idAgencia`, `nombreAgencia`, `correoAgencia`, `rucAgencia`, `razonsocialAgencia`, `ubigeoAgencia`, `direccionAgencia`, `valoracionAgencia`, `descripcionAgencia`, `telefonoAgencia`, `diaModAgencia`, `mesModAgencia`, `anioModAgencia`, `idAdmin`, `contrasenaAgencia`, `estadoAgencia`) VALUES 
+('001', 'Colca Tours SA', 'colcaT@gmail.com', '2345122', 'Viajes y muestra de la diversidad cultural del pais', '1234656312', 'av. el sol 342', '3', 'La agencia consta con terminal propio y una atencion y flota de buses de primera', '239812', 12, 12, 2017, '2', 'universo123', '1'),
+('002', 'Colca Travels SA', 'tarvelColca@gmail.com', '43451244', 'Especialista en viajescon fines culturales ', '12335632', 'av. las torres 202', '2', 'La agencia consta con terminal propio y una atencion de primera', '258412', 12, 18, 2017, '3', 'universo', '1'),
+('003', 'Aquarius Travel', 'acuariustravel@gmail.com', '83492321', 'Aventura extrema a su alcance', '49392912', 'av. los incas 112', 3, 'Agencia consultar nuestros destinos en nuestra portal web, '72132321', 11, 10, 2016, 5, 'qwerty', '1'),
 
 -- --------------------------------------------------------
 
