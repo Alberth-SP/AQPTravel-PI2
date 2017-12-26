9a8d62e5b5a4d8580d89a5c29f6292199e9f3638
davidhq0 <davidhq0@gmail.com>
2017-12-25 21:15:28 -0500

    Aserts in AdminTest

--- src/main/java/com/example/test/AdminTest.java
+++ src/main/java/com/example/test/AdminTest.java
@@ -2,21 +2,26 @@ package com.example.test;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
 
 import com.example.dao.AdminDao;
 import com.example.dao.AdminDaoImpl;
 import com.example.logic.Admin;
 
 public class AdminTest {
-
+	
+	AdminDao admin;
+	Admin ad;
+	
+	@Before
+	public void before() {
+		admin=new AdminDaoImpl();
+		ad=new Admin();
+	}
 	@Test
 	public void testFindAdminById() {
 		
-		AdminDaoImpl resultado = null;
-		AdminDaoImpl esperado = null;
-		
-		assertEquals(esperado, resultado);
+		assertEquals(admin.findAdminById(0),null);
 	}
 
 }
